61ee49efb3ea9834049871b0d93d93d7fe5aab42
Josh Holder <josh.holder@theguardian.com>
2018-01-15 15:44:28 +0000

    Lots

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
 // javascript goes here
+
+document.querySelector("#countries-dropdown").addEventListener("change", (e) => {
+	const newCountry = e.srcElement.value;
+
+	document.body.className = "select-" + newCountry;
+});
--- src/render.js
+++ src/render.js
@@ -1,27 +1,72 @@
 import templateHTML from "./src/templates/main.html!text"
 import medalTableJson from "../src/assets/data/medalsTable.json"
-import medalListByDisciplineJson from "../src/assets/data/medalsListByDiscipline.json"
+import medalListByDisciplineJson from "../src/assets/data/medalsList.json"
 import countryPerformanceJson from "../src/assets/data/performance.json"
 import rp from "request-promise"
 import Mustache from "mustache"
 import * as d3 from "d3"
+import fs from "fs"
 
 const maxDiff = d3.max(countryPerformanceJson, d => Math.abs(d.diff));
 
 const scale = d3.scaleLinear().domain([0, maxDiff]).range([0, 50]);
 
+const countries = [
+    ["Great Britain", "GBR"],
+    ["Russia", "RUS"],
+    ["Norway", "NOR"],
+    ["Germany", "GER"]
+];
+
 const countryPerformanceJsonToRender = countryPerformanceJson.map((country) => {
-	country.percentage = scale(Math.abs(country.diff));
-	country.class = country.diff >= 0 ? "positive" : "negative";
-	country.displayDiff = (country.diff > 0) ? "+" + country.diff : country.diff;
-	country.margin = country.diff >= 0 ? 50 : 50 - country.percentage;
-	return country;
+    country.percentage = scale(Math.abs(country.diff));
+    country.class = country.diff >= 0 ? "positive" : "negative";
+    country.displayDiff = (country.diff > 0) ? "+" + country.diff : country.diff;
+    country.margin = country.diff >= 0 ? 50 : 50 - country.percentage;
+    return country;
+});
+
+const nestedMedalsByDiscipline = d3.nest()
+    .key(d => d.discipline.name)
+    .key(d => d.olympicEvent.name)
+    .rollup(leaves => {
+        return {
+            "gold": leaves.filter(d => d.medalWon === "gold"),
+            "silver": leaves.filter(d => d.medalWon === "silver"),
+            "bronze": leaves.filter(d => d.medalWon === "bronze")
+        }
+    })
+    .entries(medalListByDisciplineJson
+        .sort((a, b) => {
+            if (a.medalWon === "gold") {
+                return -1;
+            } else if (a.medalWon === "silver" && b.medalWon === "bronze") {
+                return -1;
+            } else {
+                return 1;
+            }
+        })
+    )
+
+// fs.writeFileSync("./src/assets/data/test.json", JSON.stringify(nestedMedalsByDiscipline))
+
+const medalTable = medalTableJson.map(country => {
+    var medals = [];
+
+    ["gold", "silver", "bronze"].forEach(type => {
+        new Array(country.medalCount[type]).fill(null).forEach(medal => {
+            medals.push(type);
+        });
+    });
+
+    return Object.assign({}, country, { medals: medals });
 });
 
 export async function render() {
     return Mustache.render(templateHTML, {
-        "countries": medalTableJson,
-        "medalsByDiscipline": medalListByDisciplineJson,
+        "countryCodes": countries,
+        "countries": medalTable,
+        "medalsByDiscipline": nestedMedalsByDiscipline,
         "performance": countryPerformanceJsonToRender.sort((a, b) => b.diff - a.diff)
     });
 }
